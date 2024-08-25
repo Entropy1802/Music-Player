326531877ee93a498a83d84aedb5008f021058fe
Aurora <auduong.301@gmail.com>
2024-08-25 21:45:14 +0700

    add function to set dynamic height

--- script.js
+++ script.js
@@ -478,3 +478,12 @@ previousButton.addEventListener("click", () => {
 
 // Shuffle Button
 shuffleButton.addEventListener("click", shuffle);
+
+// View height display on Chrome (for Phone)
+const setFullHeight = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
+
+window.addEventListener("resize", setFullHeight);
+setFullHeight();
