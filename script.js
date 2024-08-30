0a52d2c319dfb964dc91b7f4d72bd2aaa088d30f
Aurora <auduong.301@gmail.com>
2024-08-30 22:39:26 +0700

    fix play() request was interrupted

--- script.js
+++ script.js
@@ -252,8 +252,10 @@ currentVolume.addEventListener("change", changeVolume);
 // Play
 const playSong = (id) => {
   const song = userData?.songs.find((song) => song.id === id);
-  audio.src = song.src;
-  audio.title = song.title;
+  if (song) {
+    audio.src = song.src;
+    audio.title = song.title;
+  }
 
   if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
     audio.currentTime = 0;
